c707048e9ab3cd216355bbe1a2c63bf3acdedd5e
sauravDutt <sauravd069@gmail.com>
2020-09-22 00:11:04 +0530

    just editted my app

--- App.js
+++ App.js
@@ -5,12 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fuck Yeah!! I just started with making my first mobile application, and i am getting the realtime results.🍻</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
