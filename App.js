8d1133f986ef47de6f5cb4bf88056afdbcebd516
sauravDutt <sauravd069@gmail.com>
2020-09-22 20:51:46 +0530

    just did some changes

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+ 
   return (
-    <View style={styles.container}>
-      <Text>Fuck Yeah!! I just started with making my first mobile application, and i am getting the realtime results.🍻</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View>
+        <TextInput />
+        <Button title = 'ADD'/> 
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
   
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
